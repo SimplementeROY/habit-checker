9cb6c7fe3cb473afe449f730c5e61e22ae9eb43f
SimplementeROY <sherwinagunias11@gmail.com>
2024-08-10 18:40:07 +0200

    Haciendo responsive la cosa

--- components/openForm/openForm.jsx
+++ components/openForm/openForm.jsx
@@ -4,7 +4,7 @@ export default function OpenFormButton ({openModal}) {
     return (
         <div className="manage-tasks">
             <h1 className="tasks-title">To-Do List</h1>
-            <button className="add-task-button button" onClick={openModal}><span>+ </span>Agregar tarea</button>
+            <button className="add-task-button button" onClick={openModal}><span>+ </span><span className='add-task-text'>Agregar tarea</span></button>
         </div>
     )
 }
